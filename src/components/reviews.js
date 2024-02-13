f3a19c8da25eeb80fca3af56aa473fcb1bc8a2b5
Alyssa Favorito <alyssafavorito2000@gmail.com>
2024-02-13 18:26:34 -0500

    reviews feature attempting to get data mapped

--- src/components/reviews.js
+++ src/components/reviews.js
@@ -1,11 +1,14 @@
 import React from "react";
 import ReviewForm from "./reviewForm";
+import Watchlist from "./watchlist";
 
-export default function Reviews({watchlists, reviews}){
+export default function Reviews(props){
+    const {watchlists, reviews, addReview} = props;
+    console.log(reviews)
     return(
         <div>
             <h4>Reviews</h4>
-            <ReviewForm />
+            <ReviewForm addReview = {addReview}/>
             {/* map out reviews */}
             {reviews.map((review, index) => (
                 <div review = {review} key = {index} className="card bg-dark text-white">
